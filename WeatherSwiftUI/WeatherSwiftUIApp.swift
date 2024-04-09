47d72a037be23dfcaaa305c3668936918502ac89
Emilia <yourbelovedemilia@yandex.ru>
2024-04-09 15:47:01 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Эмилия on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
